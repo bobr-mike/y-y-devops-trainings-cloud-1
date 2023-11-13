b3751340a0ea893d3106b176b5995f2a17deef17
Бобров Михаил <m.bobrov@s7.ru>
2023-11-13 09:37:15 +0700

    terra

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "yandex" {
-  service_account_key_file = "./tf_key.json"
-  folder_id                = local.folder_id
+  service_account_key_file = "/home/GROUPS7/m.bobrov/terra/yc_terraform.json"
+  folder_id                = "b1gq9bf3fsaruca2p7t8"
   zone                     = "ru-central1-a"
 }
 
@@ -26,9 +26,9 @@ resource "yandex_container_registry" "registry1" {
 }
 
 locals {
-  folder_id = "<INSERT YOUR FOLDER ID>"
+  folder_id = "b1gq9bf3fsaruca2p7t8"
   service-accounts = toset([
-    "catgpt-sa",
+    "bobr-catgpt-sa",
   ])
   catgpt-sa-roles = toset([
     "container-registry.images.puller",
@@ -42,7 +42,7 @@ resource "yandex_iam_service_account" "service-accounts" {
 resource "yandex_resourcemanager_folder_iam_member" "catgpt-roles" {
   for_each  = local.catgpt-sa-roles
   folder_id = local.folder_id
-  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["catgpt-sa"].id}"
+  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["bobr-catgpt-sa"].id}"
   role      = each.key
 }
 
@@ -50,31 +50,61 @@ data "yandex_compute_image" "coi" {
   family = "container-optimized-image"
 }
 resource "yandex_compute_instance" "catgpt-1" {
-    platform_id        = "standard-v2"
-    service_account_id = yandex_iam_service_account.service-accounts["catgpt-sa"].id
-    resources {
-      cores         = 2
-      memory        = 1
-      core_fraction = 5
+  platform_id        = "standard-v2"
+  service_account_id = yandex_iam_service_account.service-accounts["bobr-catgpt-sa"].id
+  resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 5
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.foo.id
+    nat       = true
+  }
+  boot_disk {
+    initialize_params {
+      type     = "network-hdd"
+      size     = "30"
+      image_id = data.yandex_compute_image.coi.id
     }
-    scheduling_policy {
-      preemptible = true
-    }
-    network_interface {
-      subnet_id = "${yandex_vpc_subnet.foo.id}"
-      nat = true
-    }
-    boot_disk {
-      initialize_params {
-        type = "network-hdd"
-        size = "30"
-        image_id = data.yandex_compute_image.coi.id
-      }
-    }
-    metadata = {
-      docker-compose = file("${path.module}/docker-compose.yaml")
-      ssh-keys  = "ubuntu:${file("~/.ssh/devops_training.pub")}"
-    }
-}
+  }
+  metadata = {
+    docker-compose = file("${path.module}/docker-compose.yaml")
+    ssh-keys       = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+    user-data      = "#cloud-config\nruncmd:\n  - wget -O - https://monitoring.api.cloud.yandex.net/monitoring/v2/unifiedAgent/config/install.sh | bash"
 
+  }
+}
+resource "yandex_compute_instance" "catgpt-2" {
+  platform_id        = "standard-v2"
+  service_account_id = yandex_iam_service_account.service-accounts["bobr-catgpt-sa"].id
+  resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 5
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.foo.id
+    nat       = true
+  }
+  boot_disk {
+    initialize_params {
+      type     = "network-hdd"
+      size     = "30"
+      image_id = data.yandex_compute_image.coi.id
+    }
+  }
+  metadata = {
+    docker-compose = file("${path.module}/docker-compose.yaml")
+    ssh-keys       = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+    user-data      = "#cloud-config\nruncmd:\n  - wget -O - https://monitoring.api.cloud.yandex.net/monitoring/v2/unifiedAgent/config/install.sh | bash"
+
+  }
+}
 
